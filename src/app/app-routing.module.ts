b6d06fba30b2ee80572108e8aadb8c3554dbb15a
Edriand <adriandato2000@gmail.com>
2023-06-27 23:31:26 +0200

    Route Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user',
+    loadChildren: () => import('./pages/user/user.module').then(u => u.UserModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'user'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
