08156a45cb8927330e602edf92f0aecdf68c6cd3
Edriand <adriandato2000@gmail.com>
2023-07-13 23:05:13 +0200

    Rest Password Full Working

--- src/app/pages/user/confirm-reset-password/confirm-reset-password.component.ts
+++ src/app/pages/user/confirm-reset-password/confirm-reset-password.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/interfaces/user.interface';
+import { CognitoService } from 'src/app/services/cognito.service';
 import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-confirm-reset-password',
@@ -10,7 +13,19 @@ import { UserService } from 'src/app/services/user.service';
 export class ConfirmResetPasswordComponent {
   user: User;
 
-  constructor(private userService: UserService) {
+  constructor(private router: Router, private cognitoService: CognitoService, private userService: UserService) {
     this.user = userService.getUser();
+  }
+
+  public forgotPasswordSubmit() {
+    this.cognitoService.forgotPasswordSubmit(this.user).then(() => {
+      this.router.navigate(['/user/login'])
+    }).catch(() => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Lo sentimos',
+        text: 'Algo ha ido mal con el reinicio de contraseña'
+      });
+    });;
   }
 }
