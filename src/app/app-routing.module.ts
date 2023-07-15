82a8db197c8c337791fde8d54435872e3910c82f
Edriand <adriandato2000@gmail.com>
2023-07-15 11:08:19 +0200

    Login Guard Finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { loginGuard } from './guards/login.guard';
+import { redirectGuard } from './guards/redirect.guard';
 
 const routes: Routes = [
   {
     path: 'user',
+    canActivate: [redirectGuard],
     loadChildren: () => import('./pages/user/user.module').then(u => u.UserModule)
   },
   {
