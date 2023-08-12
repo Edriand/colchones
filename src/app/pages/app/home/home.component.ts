53e7c4bbe27f7435b05c3d441be4d689f30482ee
Edriand <adriandato2000@gmail.com>
2023-08-12 16:58:40 +0200

    Mapbox libraries updated

--- src/app/pages/app/home/home.component.ts
+++ src/app/pages/app/home/home.component.ts
@@ -8,14 +8,28 @@ import * as mapboxgl from 'mapbox-gl';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  map!: mapboxgl.Map;
+  marker!: mapboxgl.Marker;
+
   ngOnInit(): void {
     (mapboxgl as any).accessToken = environment.mapboxApi;
     
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [-0.37680965553121004, 39.467434152717075],
       zoom: 16
     });
+
+    this.marker = new mapboxgl.Marker({
+      draggable: true
+    });
+    
+    this.map.on('click', this.add_marker.bind(this));
+  }
+
+  add_marker(event: any) {
+    var coordinates = event.lngLat;
+    this.marker.setLngLat(coordinates).addTo(this.map);
   }
 }
